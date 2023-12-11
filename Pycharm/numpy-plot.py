b7ef3c28b044c8fe2663271aa8e0df6f87718794
MANOJ B <84579084+ManojB26@users.noreply.github.com>
2023-12-11 13:01:53 +0530

    commit

--- Pycharm/numpy-plot.py
+++ Pycharm/numpy-plot.py
@@ -8,3 +8,13 @@ plt.show()
 
 B = np.zeros((3, 3), dtype=int)
 print(B)
+
+L = []
+for x in range(0, 100):
+    L.append(x * 2)
+print(L)
+print(L[::-2])
+print(L[:2])
+
+L1 = list(range(0,10,2))
+print(L1)
